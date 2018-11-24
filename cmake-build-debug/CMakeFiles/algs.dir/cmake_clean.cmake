acdf97f6a6c8dafbc4e93b3ca3ebb2256046f3f9
traceMing <rmqi@live.com>
2018-11-24 22:39:56 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/algs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algs.dir/main.cpp.o"
+  "bin/algs.pdb"
+  "bin/algs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
